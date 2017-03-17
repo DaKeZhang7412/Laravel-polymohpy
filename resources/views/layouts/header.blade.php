a1531b0f1de8e55e1f0177da36593e8bb8882ebf
DaKeZhang7412 <tribute.zhang@gmail.com>
2017-03-17 10:46:14 -0500

    first

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,19 @@
+<head>
+    <title>Edmond Great Admin</title>
+    <meta charset="UTF-8') !!}
+    <meta name="viewport" content="width=device-width, initial-scale=1.0') !!}
+    {!! Html::style('css/bootstrap.min.css') !!}
+    {!! Html::style('css/bootstrap-responsive.min.css') !!}
+    {!! Html::style('css/fullcalendar.css') !!}
+    {!! Html::style('css/matrix-style.css') !!}
+    {!! Html::style('css/matrix-media.css') !!}
+    {!! Html::style('font-awesome/css/font-awesome.css') !!}
+    {!! Html::style('css/jquery.gritter.css') !!}
+    {!! Html::style('http://fonts.googleapis.com/css?family=Open+Sans:400,700,800') !!}
+</head>
+
+<!--Header-part-->
+<div id="header">
+ <h1><a href="dashboard.html">Great Wall Admin</a></h1>
+</div>
+<!--close-Header-part-->
