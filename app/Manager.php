0da879aaa3f666910913db92362d9500ba6290c7
DaKeZhang7412 <tribute.zhang@gmail.com>
2017-03-18 10:31:37 -0500

    no message

--- app/Manager.php
+++ app/Manager.php
@@ -4,7 +4,7 @@ namespace restaurant;
 
 use Illuminate\Database\Eloquent\Model;
 
-class AdvanceUser extends User
+class Manager extends User
 {
     // name, id, password, time-tracking, pay rate, etc.
     //store manager, run different report; has all the access.
--- app/Order.php
+++ app/Order.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //Type(dine-in, pick-up, delivery), customerId, courseId list, payment part(sub total, tax, delivery fee, discount, etc.), Status(confirmed, printed, paid.)
+    //Type(dine-in, pick-up, delivery), customerId, staff who created this order; courseId list, payment part(sub total, tax, delivery fee, discount, etc.), Status(confirmed, printed, paid.)
+    public function customer(){
+        return $this->belongsTo('App\Customer');
+    }
 }
